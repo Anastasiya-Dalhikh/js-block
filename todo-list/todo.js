0a851e0d86549e829cb95b33145043906e11ab83
Anastasiya-Dalhikh <snierzana@gmail.com>
2026-08-15 12:29:02 +0300

    add function to create el and create todo function

--- todo-list/todo.js
+++ todo-list/todo.js
@@ -1,92 +1,126 @@
 const root = document.querySelector('#root');
 
-const divWrapper = document.createElement('div');
-divWrapper.classList.add('todo-container');
+
+function createEl(tag, options = {}) {
+    const el = document.createElement(tag);
+
+    if(options.classes){
+        el.classList.add(options.classes);
+    }
+
+    if (options.text) {
+        el.textContent = options.text;
+    }
+
+    if (options.placeholder) {
+        el.placeholder = options.placeholder;
+    }    
+    if (options.type){
+        el.type = options.type;
+    } 
+    if (options.value){
+        el.value = options.value;
+    } 
+
+    if (options.children) {
+        options.children.forEach(child => {
+            el.append(child);
+        });
+    }
+    
+    if (options.parent) {
+        options.parent.append(el);
+    }
+    
+    return el;
+}
+
+const divWrapper = createEl('div',{
+    classes: 'todo-container',
+    // parent: root
+});
+
+const container = createEl('div', {
+    classes: 'container',
+    parent: divWrapper
+});
+
+const todoSection = createEl('div', {
+    classes: 'todo-section',
+    parent: container
+});
+
+const deleteAllBtn = createEl('button', {
+    classes: 'delete-all-btn',
+    text: 'Delete All',
+    parent: todoSection
+});
+
+const input = createEl('input', {
+    classes: 'main-input',
+    placeholder: 'Enter todo ...',
+    type: 'text',
+    parent: todoSection
+});
+
+const addBtn = createEl('button', {
+    classes: 'add-btn',
+    text: 'Add',
+    parent: todoSection
+});
+
+
+
+function createTodo(text, date = ''){
+    const todo1 = createEl('div', {
+    classes: 'first-todo-container',
+    // parent: container
+});
+
+const checkbox = createEl('input', {
+    classes: 'todo-checkbox',
+    type: 'checkbox',
+
+});
+
+const customCheckbox = createEl('span', {
+    classes: 'custom-checkmark',
+        
+});
+
+const checkboxLabel = createEl('label', {
+    classes: 'todo-checkbox-label',
+    children: [checkbox, customCheckbox],
+    parent: todo1
+});
+
+const textSpan = createEl('span',{
+    classes: 'todo-text',
+    text: text,
+    parent: todo1
+}); 
+
+const deleteBtn = createEl('button',{
+    classes: 'todo-delete-btn',
+    text: 'x',
+    parent: todo1
+});
+
+const dateInput = createEl('input',{
+    classes: 'todo-date',
+    type: 'date',
+    value: date,
+    parent: todo1
+});
+
+return todo1;
+
+}
 
 root.append(divWrapper);
 
-const container = document.createElement('div');
-container.classList.add('container');
-
-divWrapper.append(container);
-
-const todoSection = document.createElement('div');
-todoSection.classList.add('todo-section');
-
-container.append(todoSection);
-
-const deleteAllBtn = document.createElement('button');
-deleteAllBtn.classList.add('delete-all-btn');
-deleteAllBtn.textContent = 'Delete All';
-
-todoSection.append(deleteAllBtn);
-
-const input = document.createElement('input');
-input.classList.add('main-input');
-input.placeholder = 'Enter todo ...';
-input.type = 'text';
-console.log(todoSection);
-
-todoSection.append(input);
-
-const addBtn = document.createElement('button');
-addBtn.classList.add('add-btn');
-addBtn.textContent = 'Add';
-
-todoSection.append(addBtn);
-
-////////////////* TODO-TEXT-ELEMENT  */////////////////
-
-const todo1 = document.createElement('div');
-todo1.classList.add('first-todo-container');
-
-container.append(todo1);
-
-
-const checkbox = document.createElement('input');
-checkbox.type = 'checkbox';
-checkbox.className = 'todo-checkbox';
-
-
-todo1.append(checkbox);
-
-
-const customCheckbox = document.createElement('span');
-customCheckbox.classList.add('custom-checkmark');
-
-
-const checkboxLabel = document.createElement('label');
-checkboxLabel.classList.add('todo-checkbox-label');
-checkboxLabel.append(checkbox);
-checkboxLabel.append(customCheckbox);
-
-todo1.append(checkboxLabel);
-
-
-const textSpan = document.createElement('span');
-textSpan.classList.add('todo-text');
-
-todo1.append(textSpan);
-
-
-const deleteBtn = document.createElement('button');
-deleteBtn.classList.add('todo-delete-btn');
-deleteBtn.textContent = 'x';
-todo1.append(deleteBtn);
-
-
-const dateInput = document.createElement('input');
-dateInput.type = 'date';
-dateInput.classList.add('todo-date');
-
-todo1.append(dateInput);
-
-
-const todo2 = todo1.cloneNode(true); 
-container.append(todo2);
-
-
-
+const newTodo = createTodo('Купить продукты');
+container.append(newTodo); 
 
 
 
